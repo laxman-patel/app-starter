23a04178a6f25d56bbaa7c0207a7760cc2fc2d79
Laxman patel <laxmanlp777@gmail.com>
2021-02-09 14:56:45 +0530

    fisrt commit

---
+++ pages/api/graphql.js
@@ -0,0 +1,13 @@
+import { ApolloServer } from "apollo-server-micro";
+import typeDefs from "../../graphql/typedefs";
+import resolvers from "../../graphql/resolvers";
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default apolloServer.createHandler({ path: "/api/graphql" });
---
+++ pages/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div className="container mx-auto px-4">hello</div>;
+};
+
+export default Home;
